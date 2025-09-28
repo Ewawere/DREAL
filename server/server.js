a22317e75a046c6df0102efb6df9a41b1ac1d3d6
Ewawere <153601310+Ewawere@users.noreply.github.com>
2025-09-28 15:19:37 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Serve static files from the public folder
-app.use(express.static(path.join(__dirname, '..', 'public'))));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(session({
   secret: 'superSecretKey',
