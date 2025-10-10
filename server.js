0e0ebd4520b06f7bef1eeacd697e7917db1137fd
Ewawere <153601310+Ewawere@users.noreply.github.com>
2025-10-10 12:22:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const loadActivationCodes = () =>
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
   secret: process.env.SESSION_SECRET || 'superSecretKey',
